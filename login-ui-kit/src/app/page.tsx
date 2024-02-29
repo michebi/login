2dc517c6d9c02fc5a508549facbf36ce58318f0a
Michael Ebimomi <mebimomi.projects@gmail.com>
2024-02-29 22:54:56 +0000

    responsive designs added

--- login-ui-kit/src/app/page.tsx
+++ login-ui-kit/src/app/page.tsx
@@ -6,14 +6,14 @@ import { FcGoogle } from "react-icons/fc";
 
 export default function Home() {
   return (
-    <main className="flex flex-row items-stretch h-screen bg-background-primary-default">  
-    {/* Main container 100% VH */}
+    <main className="flex flex-row md:flex-row items-stretch h-screen bg-background-primary-default">
+      {/* Main container 100% VH */}
 
-        {/* Left section */}
-        <div className="flex w-full flex-1 justify-center items-center">
+      {/* Left section */}
+      <div className="flex w-full md:w-1/2 lg:flex-1 justify-center items-center">
 
         {/* Left Body */}
-        <div className="flex flex-col justify-center gap-8 mx-8 min-w-[416px]">
+        <div className="flex flex-col justify-center w-full gap-8 mx-8 max-w-[416px]">
 
           {/* Login Title */}
           <h1 className="text-2xl font-medium text-font-primary text-center">Login into Michebi</h1> 
@@ -21,43 +21,33 @@ export default function Home() {
           {/* Form Container  */}
           <div className="flex flex-col justify-center items-stretch gap-4">
 
-          {/* Email Input Field */}
-          <InputField
-            label="Email"
-            name="email-input"
-            type="email"
-            isError={false}
-          />
+            {/* Email Input Field */}
+            <InputField
+              label="Email"
+              name="email-input"
+              type="email"
+              isError={false}
+            />
 
-          {/* Password Input Field - Hidden for design layout
-          <InputField
-            label="Password"
-            name="password-input"
-            type="password"
-            isError={false}
-          /> */}
+            {/* Button Component */}
+            <Button variant="primary">Continue</Button>
 
-          {/* Button Component */}
-          <Button 
-          variant="primary">Continue</Button>
-
-          <div className="flex items-center gap-4">
-            <Divider color="gray-200" thickness="1" className="flex-1" />
-              <span className="text-font-secondary text-sm whitespace-nowrap">or</span>
-            <Divider color="gray-200" thickness="1" className="flex-1" />
-          </div>
-            <Button variant="outline" icon={<FcGoogle size={24} />}>
-              Sign in with Google
-            </Button>
-              </div>
+            <div className="flex items-center gap-4">
+              <Divider color="gray-200" thickness="1" className="flex-1" />
+                <span className="text-font-secondary text-sm whitespace-nowrap">or</span>
+              <Divider color="gray-200" thickness="1" className="flex-1" />
             </div>
+              <Button variant="outline" icon={<FcGoogle size={24} />}>
+                Sign in with Google
+              </Button>
           </div>
+        </div>
+      </div>
       
-        {/* Right section */}
-        <div className="flex w-full flex-1 justify-center bg-neutral-900">
-          </div>
+      {/* Right section - hidden on mobile, displayed from md breakpoint and up */}
+      <div className="hidden md:flex w-full md:w-1/2 lg:flex-1 justify-center bg-neutral-900">
+        {/* Right section content */}
+      </div>
     </main>
   );
 }
-
-
