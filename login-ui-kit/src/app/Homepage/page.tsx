3dbc5d94c7a7449153f1e4d2a8a517da50691c08
Michael Ebimomi <mebimomi.projects@gmail.com>
2024-04-09 01:45:47 +0100

    Fixed Lottie animation - Removed Lottie package and used react-lottie

--- login-ui-kit/src/app/Homepage/page.tsx
+++ login-ui-kit/src/app/Homepage/page.tsx
@@ -1,22 +1,16 @@
 'use client'
-
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import lottieanimation from "./lottieanimation.json"
 import React from 'react';
+import Lottie from "lottie-react";
 
 
 export default function Homepage() {
   return (
     <div className="h-screen bg-background-primary-default w-full flex items-center justify-center">
-      <div className="flex flex-col justify-center max-w-[416px] text-center gap-2">
-        <Player
-          autoplay
-          loop = {false}
-          src="src/app/Homepage/lottieanimation.json"
-          style={{ height: '150px', width: '150x' }}
-        >
-          <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-        </Player>
+      <div className="flex flex-col justify-center max-w-[416px] text-center gap-2 items-center justify-center">
+        <div className="w-40 h-40">
+          <Lottie animationData={lottieanimation} loop={false} />
+        </div>
         <div className="text-font-primary font-medium text-xl">
           You have successfully logged in!
         </div>
